013cfbfdcb594de8be6b6f2d16506d3ec5872f4b
Theekshanahansamal2007 <94382409+Theekshanahansamal2007@users.noreply.github.com>
2022-07-21 00:22:00 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,31 +19,31 @@ global.APIKeys = {
 
 //customize settings\\
 
-global.alivemsg = `My name is RED DRAGON🔥
-My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
+global.alivemsg = `My name is TIKKᗩ ᗷOT🔥
+My Creator is theekshana😎` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['+94741844543'] //Owner number in aive msg
+global.premium = ['+94741844543'] //Owner Number info
+global.ownernomer = '+94741844543' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
-global.botname = ' ☬RED༒DRAGON࿐' //Bot Name
+global.ownername = '😎TᕼEEKᔕᕼᗩᑎᗩ' //Owner Name
+global.botname = ' ☬TIKKᗩ༒ᗷOT࿐' //Bot Name
 
-global.button = '☰ GITHUB ☰' //Costomize A Button Name In Alive Message
-global.btnurl = 'https://github.com/prabhasha2006' //Change Your Button's URL
+global.button = '☰ YOᑌTᑌᗷE ☰' //Costomize A Button Name In Alive Message
+global.btnurl = 'https://youtube.com/channel/UCNa4OjpD2diX_Z7hFnM9sjg' //Change Your Button's URL
 
 global.region = 'India, South India, Kerala' //Country Region
 
-global.packname = '☬RED༒DRAGON࿐' //Bot Pacage Name
-global.author = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯' //Author Name
+global.packname = '☬TIKKᗩ༒ᗷOT࿐' //Bot Pacage Name
+global.author = '😎TᕼEEKᔕᕼᗩᑎᗩ❯❯' //Author Name
 global.prefa = ['','!','.','😂','❤️','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
 global.sessionName = 'session' //Session Name!
 global.sp = '⭔' //A Charactor
 global.dragonemoji = '🧩' // Menu emoji
-global.footer = '©_Kumuthu.' //In list menu
+global.footer = '©_TIKKᗩ.' //In list menu
 global.chatbotpre = 'true'  // Chat bot true/false
 
-global.ig = 'https://github.com/prabhasha2006' //Github Account!
+global.ig = 'https://github.com/theekshanahansamal2007' //Github Account!
 
 global.mess = { // COSTOMIZE WITH YOUR LANGUAGE
 	
